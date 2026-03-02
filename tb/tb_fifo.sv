1ae3776f674f66cf502a551b9b9d7057c30d9bf7
Viktor Krushinsky <vittorio.kru@yandex.ru>
2026-03-02 10:40:22 +0300

    feat: Не работает проверка assert, написан тест

--- tb/tb_fifo.sv
+++ tb/tb_fifo.sv
@@ -26,10 +26,13 @@ module tb_fifo();
   // TB reg model
   logic [DATA_WIDTH_OUT - 1: 0]    ram1 [$: RAM_DEPTH - 1];
   logic [DATA_WIDTH_OUT - 1: 0]    ram2 [$: RAM_DEPTH - 1];
+  logic [DATA_WIDTH_OUT - 1: 0]    ref_tdata1;
+  logic [DATA_WIDTH_OUT - 1: 0]    ref_tdata2;
+
 
   task automatic reset_gen();
     aresetn <= 1'b0;
-    #(2*CLK_PERIOD);
+    @(posedge clk);
     aresetn <= 1'b1;
   endtask
 
@@ -87,9 +90,13 @@ multi_port_fifo
   // Подача сброса, завершение симуляции
   initial begin
     reset_gen();
-    repeat(600)
-      #(2*CLK_PERIOD);
+    repeat(600) @(posedge clk);
     $finish();
+  end
+
+  initial begin
+    wait(aresetn);
+    repeat(100) test_read_without_both();
   end
 
   // Блок отправки 64-битных транзакций
@@ -108,7 +115,7 @@ multi_port_fifo
       sleep_time  = $urandom_range(0, 50);
       data_amount = $urandom_range(0, FIFO_DEPTH);
       repeat (data_amount) push_data();
-      repeat (sleep_time ) #(2*CLK_PERIOD);
+      repeat (sleep_time ) @(posedge clk);
     end
   end
 
@@ -119,25 +126,71 @@ multi_port_fifo
         ram2.push_front(s_tdata[DATA_WIDTH_IN   - 1 : DATA_WIDTH_IN/2]);
         $display($sformatf("Ram1 size is: %d", ram1.size()));
       end
-      #(2*CLK_PERIOD);
+      @(posedge clk);
     end
   end
 
   task read1();
-    m_tready1 <= '1;
+    m_tready1 <= 1'b1;
     wait(m_tready1 && m_tvalid1);
-    m_tready1 <= '0;
+    ref_tdata1 = ram1.pop_back();
+    @(posedge clk);
+    m_tready1 <= 1'b0;
   endtask
   task read2();
-    m_tready2 <= '2;
+    m_tready2 <= 1'b1;
     wait(m_tready2 && m_tvalid2);
-    m_tready2 <= '0;
+    ref_tdata2 = ram2.pop_back();
+    @(posedge clk);
+    m_tready2 <= 1'b0;
+  endtask
+  task read_last();
+    if (m_tuser1)
+      read1();
+    else if (m_tuser2)
+      read2();
+  endtask
+  task read_both();
+    fork
+      read1();
+      read2();
+    join
+  endtask
+
+  // Tests
+  task test_read_without_both();
+    read_last();
+  endtask
+
+  task test_read_both();
+    wait(m_tvalid1 && m_tvalid2);
+    read_both();
+  endtask
+
+  task test_read_random();
+    bit is_both_read;
+    std::randomize(is_both_read);
+
+    if (is_both_read) begin
+      wait(m_tvalid1 && m_tvalid2);
+      read_both();
+    end
+    else begin
+      read_last();
+    end
   endtask
 
   // При заполненном FIFO сигнал tvalid не может быть в 1
   assert property (@(posedge clk) (ram1.size() == RAM_DEPTH || ram2.size() == RAM_DEPTH) |=> ~s_tready);
 
   // При чтении из FIFO данные соответствуют эталонной модели
-  assert property (@(posedge clk) (m_tready1 && m_tvalid1) |-> ram1.pop_back() === m_tdata1);
-  assert property (@(posedge clk) (m_tready2 && m_tvalid2) |-> ram2.pop_back() === m_tdata2);
+  assert property
+    (@(posedge clk) (m_tready1 && m_tvalid1) |-> ref_tdata1 === $sampled(m_tdata1))
+  else
+    $error("Ref data: %h not match act data: %h", ref_tdata1, $sampled(m_tdata1));
+
+  // assert property
+  //   (@(posedge clk) (m_tready2 && m_tvalid2) |-> ref_tdata2 === $sampled(m_tdata2))
+  // else
+  //   $error("Ref data: %h not match act data: %h", ref_tdata2, $sampled(m_tdata2));
 endmodule
